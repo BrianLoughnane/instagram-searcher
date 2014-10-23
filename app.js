73e0e4bbab9b5db3822bdabbc3d1083a65436287
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-23 11:45:34 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var myApp = angular.module('myApp', ['ngAnimate']);
+myApp.controller('MyController', function($scope, $http) {
+	$scope.results = [];
+	$scope.submit = function() {
+		var tag = $scope.searchTerm;
+		var tagString = tag.toString();
+		var config = {
+			url: 'https://api.instagram.com/v1/tags/'+  tagString +'/media/recent',
+			method: 'GET',
+			callback: 'JSON_CALLBACK',
+			client_id: '161eb0c438eb4699b7e00114e0dc0be0'
+		};
+
+		$scope.searchTerm = '';
+		$scope.tag = tag;
+		$scope.submitted = true;
+		$scope.resolved = false;
+		$scope.failed = false;
+
+		$http(config).success(function(response) {
+				$scope.resolved = true;
+				console.log(response);
+				// for (var i in response) {
+
+				// 	// $scope.results.push(response.data);					
+				// }
+			}).error(function(message) {
+				$scope.failed = true;
+			});
+
+	}
+	
+});
